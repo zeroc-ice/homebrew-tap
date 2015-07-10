ddccbd57eea9b21ee794bad3bf121f3be48d6030
Joe George <joe@zeroc.com>
2015-07-10 16:49:39 -0400

    Update berkeley-db53 java configuration

--- Formula/berkeley-db53.rb
+++ Formula/berkeley-db53.rb
@@ -11,14 +11,7 @@ class BerkeleyDb53 < Formula
     sha256 "12d644ecddfec4c982bb9ba59dad9a6747a0f1dcb216d5803eb60669e474bb41" => :yosemite
   end
 
-  option "with-java-8", "Compile with Java 8 support."
-  option "without-java", "Compile without Java support."
-
-  if build.with? "java-8"
-    depends_on :java => "1.8"
-  elsif build.with? "java"
-    depends_on :java => "1.7"
-  end
+  depends_on :java => :recommended
 
   # Fix build under Xcode 4.6
   # Double-underscore names are reserved, and __atomic_compare_exchange is now
@@ -38,31 +31,7 @@ class BerkeleyDb53 < Formula
       --enable-cxx
     ]
 
-    if build.with? "java" or build.with? "java-8"
-      if build.with? "java-8"
-        java_home = ENV["JAVA_HOME"] = `/usr/libexec/java_home -v 1.8`.chomp
-      else
-        java_home = ENV["JAVA_HOME"] = `/usr/libexec/java_home -v 1.7`.chomp
-      end
-
-      # The Oracle JDK puts jni.h into #{java_home}/include and jni_md.h into
-      # #{java_home}/include/darwin.  The original Apple/SUN JDK placed jni.h
-      # and jni_md.h into
-      # /System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/
-      #
-      # Setup the include path with the Oracle JDK location first and the Apple JDK location second.
-      ENV.append("CPPFLAGS", "-I#{java_home}/include")
-      ENV.append("CPPFLAGS", "-I#{java_home}/include/darwin")
-      ENV.append("CPPFLAGS", "-I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/")
-
-      # This doesn't work at present.
-      #ENV.O3
-      #ENV.append_to_cflags("-I#{java_home}/include")
-      #ENV.append_to_cflags("-I#{java_home}/include/darwin")
-      #ENV.append_to_cflags("-I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/")
-
-      args << "--enable-java"
-    end
+    args << "--enable-java" if build.with? "java"
 
     # BerkeleyDB requires you to build everything from the build_unix subdirectory
     cd "build_unix" do
