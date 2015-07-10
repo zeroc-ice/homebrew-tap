e05e76fdcd5581fd86374668972581ab9d243998
Joe George <joe@zeroc.com>
2015-07-10 11:04:59 -0400

    Ice Touch formula updates

--- Formula/icetouch-head.rb
+++ Formula/icetouch-head.rb
@@ -1,4 +1,5 @@
 class IcetouchHead < Formula
+  desc "Implementation of Ice for iOS and OS X targeting Xcode development"
   homepage "https://zeroc.com"
   head "https://github.com/zeroc-ice/icetouch.git"
 
@@ -8,14 +9,44 @@ class IcetouchHead < Formula
     # Unset ICE_HOME as it interferes with the build
     ENV.delete("ICE_HOME")
     ENV.delete("USE_BIN_DIST")
-    ENV.delete("CPPFLAGS")
     ENV.O2
 
-    args = %W[
-      prefix=#{prefix}
-      OPTIMIZE=yes
-    ]
+    system "make", "install", "OPTIMIZE=yes", "prefix=#{prefix}"
+  end
 
-    system "make", "install", *args
+  test do
+    (testpath/"Hello.ice").write <<-EOS.undent
+      module Test {
+        interface Hello {
+          void sayHello();
+        };
+      };
+    EOS
+    (testpath/"Test.cpp").write <<-EOS.undent
+      #include <Ice/Ice.h>
+      #include <Hello.h>
+
+      class HelloI : public Test::Hello {
+      public:
+        virtual void sayHello(const Ice::Current&) {}
+      };
+
+      int main(int argc, char* argv[]) {
+        Ice::CommunicatorPtr communicator;
+        communicator = Ice::initialize(argc, argv);
+        Ice::ObjectAdapterPtr adapter =
+            communicator->createObjectAdapterWithEndpoints("Hello", "default -h localhost -p 10000");
+        adapter->add(new HelloI, communicator->stringToIdentity("hello"));
+        adapter->activate();
+        communicator->destroy();
+        return 0;
+      }
+    EOS
+    system "#{lib}/IceTouch/Cpp/bin/slice2cpp", "hello.ice"
+    system "xcrun", "--sdk", "macosx", "clang++", "-c", "-I#{lib}/IceTouch/Cpp/macosx.sdk/usr/include", "-I.", "Hello.cpp"
+    system "xcrun", "--sdk", "macosx", "clang++", "-c", "-I#{lib}/IceTouch/Cpp/macosx.sdk/usr/include", "-I.", "Test.cpp"
+    system "xcrun", "--sdk", "macosx", "clang++", "-L#{lib}/IceTouch/Cpp/macosx.sdk/usr/lib", "-o", "test", "Test.o", \
+      "Hello.o", "-lIce", "-framework", "Security", "-framework", "Foundation", "-lbz2", "-liconv"
+    system "./test", "--Ice.InitPlugins=0"
   end
 end
--- Formula/icetouch36.rb
+++ Formula/icetouch36.rb
@@ -1,7 +1,8 @@
 class Icetouch36 < Formula
+  desc "Implementation of Ice for iOS and OS X targeting Xcode development"
   homepage "https://zeroc.com"
-
-  url "https://github.com/zeroc-ice/icetouch.git", :tag => "v3.6.0"
+  url "https://github.com/zeroc-ice/icetouch.git",
+    :tag => "v3.6.0", :revision => "c39885c589e3c8ecefcfe456ac5c83091b09f927"
 
   bottle do
     root_url "https://zeroc.com/download/homebrew/bottles"
@@ -14,14 +15,44 @@ class Icetouch36 < Formula
     # Unset ICE_HOME as it interferes with the build
     ENV.delete("ICE_HOME")
     ENV.delete("USE_BIN_DIST")
-    ENV.delete("CPPFLAGS")
     ENV.O2
 
-    args = %W[
-      prefix=#{prefix}
-      OPTIMIZE=yes
-    ]
+    system "make", "install", "OPTIMIZE=yes", "prefix=#{prefix}"
+  end
 
-    system "make", "install", *args
+  test do
+    (testpath/"Hello.ice").write <<-EOS.undent
+      module Test {
+        interface Hello {
+          void sayHello();
+        };
+      };
+    EOS
+    (testpath/"Test.cpp").write <<-EOS.undent
+      #include <Ice/Ice.h>
+      #include <Hello.h>
+
+      class HelloI : public Test::Hello {
+      public:
+        virtual void sayHello(const Ice::Current&) {}
+      };
+
+      int main(int argc, char* argv[]) {
+        Ice::CommunicatorPtr communicator;
+        communicator = Ice::initialize(argc, argv);
+        Ice::ObjectAdapterPtr adapter =
+            communicator->createObjectAdapterWithEndpoints("Hello", "default -h localhost -p 10000");
+        adapter->add(new HelloI, communicator->stringToIdentity("hello"));
+        adapter->activate();
+        communicator->destroy();
+        return 0;
+      }
+    EOS
+    system "#{lib}/IceTouch/Cpp/bin/slice2cpp", "hello.ice"
+    system "xcrun", "--sdk", "macosx", "clang++", "-c", "-I#{lib}/IceTouch/Cpp/macosx.sdk/usr/include", "-I.", "Hello.cpp"
+    system "xcrun", "--sdk", "macosx", "clang++", "-c", "-I#{lib}/IceTouch/Cpp/macosx.sdk/usr/include", "-I.", "Test.cpp"
+    system "xcrun", "--sdk", "macosx", "clang++", "-L#{lib}/IceTouch/Cpp/macosx.sdk/usr/lib", "-o", "test", "Test.o", \
+      "Hello.o", "-lIce", "-framework", "Security", "-framework", "Foundation", "-lbz2", "-liconv"
+    system "./test", "--Ice.InitPlugins=0"
   end
 end
