0406ed9175b9c74c962a5561d5da6541d3b57dbc
Joe George <joe@zeroc.com>
2016-04-05 14:54:16 -0400

    berkeley-db53 formula cleanup

--- Formula/berkeley-db53.rb
+++ Formula/berkeley-db53.rb
@@ -1,9 +1,8 @@
 class BerkeleyDb53 < Formula
+  desc "High performance key/value database"
   homepage "http://www.oracle.com/technology/products/berkeley-db/index.html"
   url "https://zeroc.com/download/homebrew/db-5.3.28.NC.brew.tar.gz"
   sha256 "8ac3014578ff9c80a823a7a8464a377281db0e12f7831f72cef1fd36cd506b94"
-
-  keg_only "Conflicts with berkeley-db in main repository."
 
   bottle do
     root_url "https://zeroc.com/download/homebrew/bottles"
@@ -12,7 +11,9 @@ class BerkeleyDb53 < Formula
     sha256 "ac370029c8a5160598f1d2171beb5a29637634827bf3b607821ef76963cdd532" => :el_capitan
   end
 
-  depends_on :java => [ "1.7+", :recommended]
+  keg_only "Conflicts with berkeley-db in main repository."
+
+  depends_on :java => ["1.7+", :recommended]
 
   def install
     # BerkeleyDB dislikes parallel builds
@@ -26,13 +27,13 @@ class BerkeleyDb53 < Formula
 
     if build.with? "java"
       args << "--enable-java"
-      inreplace "dist/Makefile.in", "@JAVACFLAGS@",  "@JAVACFLAGS@ -source 1.7 -target 1.7"
+      inreplace "dist/Makefile.in", "@JAVACFLAGS@", "@JAVACFLAGS@ -source 1.7 -target 1.7"
     end
 
     # BerkeleyDB requires you to build everything from the build_unix subdirectory
     cd "build_unix" do
       system "../dist/configure", *args
-      system "make install"
+      system "make", "install"
 
       # use the standard docs location
       doc.parent.mkpath
