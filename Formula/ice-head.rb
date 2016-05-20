171520b43cc198c30966e6ac4b8b556693572a08
Benoit Foucher <benoit@zeroc.com>
2016-05-20 10:58:41 +0200

    Fixed ice-head formula to support new build system

--- Formula/ice-head.rb
+++ Formula/ice-head.rb
@@ -5,14 +5,12 @@ class IceHead < Formula
 
   option "with-java", "Build Ice for Java and the IceGrid Admin app"
 
-  depends_on "berkeley-db53"
-  depends_on "lmdb"
   depends_on "mcpp"
-  depends_on :java => ["1.7+", :optional]
+  depends_on "lmdb"
+  depends_on :java => [ "1.8+", :optional]
   depends_on :macos => :mavericks
 
   def install
-     inreplace "cpp/src/slice2js/Makefile", /install:/, "dontinstall:"
 
     # Unset ICE_HOME as it interferes with the build
     ENV.delete("ICE_HOME")
@@ -20,38 +18,18 @@ class IceHead < Formula
     ENV.delete("CPPFLAGS")
     ENV.O2
 
-    args = %W[
-      prefix=#{prefix}
-      embedded_runpath_prefix=#{prefix}
-      USR_DIR_INSTALL=yes
-      OPTIMIZE=yes
-      MCPP_HOME=#{Formula["mcpp"].opt_prefix}
-      DB_HOME=#{Formula["berkeley-db53"].opt_prefix}
+    args = [
+      "prefix=#{prefix}",
+      "embedded_runpath_prefix=#{prefix}",
+      "install_phpdir=#{share}/php",
+      "install_phplibdir=#{lib}/php/extensions",
+      "OPTIMIZE=yes",
+      "MCPP_HOME=#{Formula["mcpp"].opt_prefix}",
+      "LMDB_HOME=#{Formula["lmdb"].opt_prefix}",
+      "CONFIGS=shared cpp11-shared",
+      "SKIP=slice2py slice2rb slice2js",
+      "LANGUAGES=cpp objective-c php #{(build.with? 'java') ? 'java' : ''}"
     ]
-
-    system "cp", "-r", "cpp", "cpp11"
-    cd "cpp11" do
-      system "make", "install", "CPP11_MAPPING=yes", *args
-    end
-
-    cd "cpp" do
-      system "make", "install", *args
-    end
-
-    cd "objective-c" do
-      system "make", "install", *args
-    end
-
-    if build.with? "java"
-      cd "java" do
-        system "make", "install", *args
-      end
-    end
-
-    cd "php" do
-      args << "install_phpdir=#{share}/php"
-      args << "install_libdir=#{lib}/php/extensions"
-      system "make", "install", *args
-    end
+    system "make", "install", *args
   end
 end
