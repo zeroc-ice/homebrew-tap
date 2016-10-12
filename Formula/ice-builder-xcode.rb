575590d6552e311ace1a22e853a98f98d133d1e7
Joe George <joe@zeroc.com>
2016-10-12 12:08:04 -0400

    Update ice-builder-xcode formula description and url

--- Formula/ice-builder-xcode.rb
+++ Formula/ice-builder-xcode.rb
@@ -1,6 +1,6 @@
 class IceBuilderXcode < Formula
-  desc "Helps compiling Slice files to C++ or Objective-C in Xcode"
-  homepage "https://github.com/zeroc-ice/ice-builder.xcode"
+  desc "Command-line tool for compiling Slice files in Xcode"
+  homepage "https://github.com/zeroc-ice/ice-builder-xcode"
   url "https://github.com/zeroc-ice/ice-builder-xcode/archive/v3.0.1.tar.gz"
   sha256 "d9206ef562473ef66ff4cc3a8a91687e982c165a9584ae4ce7ca786ca3daef89"
 
