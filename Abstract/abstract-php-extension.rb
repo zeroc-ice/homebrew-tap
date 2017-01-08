2513c0562f0cee4f246eaca4131cbd0aa1b553df
Joe George <joe@zeroc.com>
2017-01-08 12:19:34 -0500

    Avoid failure when tapping and homebrew/php not already tapped

--- Abstract/abstract-php-extension.rb
+++ Abstract/abstract-php-extension.rb
@@ -15,6 +15,20 @@ phpTapDirectory = shell_output("brew --repo homebrew/homebrew-php").strip
 if File.exists?(phpTapDirectory)
     require File.join(phpTapDirectory, "Abstract", "abstract-php-extension")
 else
-    puts "php Homebrew tap missing. You need to install the hombrew-php tap. Please run 'brew tap homebrew/homebrew-php'"
-    exit(0)
+    puts "PHP Homebrew tap missing. This is required to install ZeroC Ice PHP based packages."
+    puts "To install please run 'brew tap homebrew/homebrew-php'"
+
+    class AbstractPhpExtension < Formula
+        def self.init
+        end
+    end
+
+    class AbstractPhp71Extension < AbstractPhpExtension
+    end
+
+    class AbstractPhp70Extension < AbstractPhpExtension
+    end
+
+    class AbstractPhp56Extension < AbstractPhpExtension
+    end
 end
