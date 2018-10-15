e7dcabed8b0d80cb463379c10b2545fda91e5975
Joe George <joe@zeroc.com>
2018-10-15 10:32:20 -0400

    Add IceGrid GUI cask

---
+++ Casks/icegridgui.rb
@@ -0,0 +1,10 @@
+cask 'icegridgui' do
+  version '3.7.1'
+  sha256 'a7a19284e2f241aa4064407c6ba1f5cd03f8e02707c7c1a34e8c86f9f856f81d'
+
+  url "https://zeroc.com/download/ice/3.7/IceGridGUI-3.7.1.dmg"
+  name 'IceGrid GUI'
+  homepage 'https://zeroc.com'
+
+  app 'IceGrid GUI.app'
+end
