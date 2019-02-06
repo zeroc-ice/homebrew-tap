c834b0f5429949a133281941bd17241d7bdf0248
Joe George <joe@zeroc.com>
2019-02-06 15:26:08 -0500

    icegrid gui 3.7.2

--- Casks/icegridgui.rb
+++ Casks/icegridgui.rb
@@ -1,8 +1,8 @@
 cask 'icegridgui' do
-  version '3.7.1'
-  sha256 'a7a19284e2f241aa4064407c6ba1f5cd03f8e02707c7c1a34e8c86f9f856f81d'
+  version '3.7.2'
+  sha256 'a16c6a3c9ac800056819f06dca5a7ef8016eb49d8560820b2dc02c7eba058add'
 
-  url "https://zeroc.com/download/ice/3.7/IceGridGUI-3.7.1.dmg"
+  url "https://zeroc.com/download/ice/3.7/IceGridGUI-3.7.2.dmg"
   name 'IceGrid GUI'
   homepage 'https://zeroc.com'
 
