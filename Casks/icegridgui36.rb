084d5a423b8935088b8dba596f7f73e92583bfd5
Joe George <joe@zeroc.com>
2019-08-13 11:13:31 -0400

    Add icegridgui36 cask

---
+++ Casks/icegridgui36.rb
@@ -0,0 +1,10 @@
+cask 'icegridgui36' do
+    version '3.6.5'
+    sha256 '04a0a964ad027b8482e02f595af8ee2bcdeb8e51781953b77a622cf35ac8aa68'
+
+    url "https://zeroc.com/download/ice/3.6/IceGridAdmin-3.6.5.dmg"
+    name 'IceGrid Admin'
+    homepage 'https://zeroc.com'
+
+    app 'IceGrid Admin.app'
+  end
