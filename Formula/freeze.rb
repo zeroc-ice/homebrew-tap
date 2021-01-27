381f9ce84ce9d0e34fe800e91afb016a0984b741
Joe George <joe@zeroc.com>
2021-01-27 09:36:13 -0500

    freeze 3.7.5

--- Formula/freeze.rb
+++ Formula/freeze.rb
@@ -1,19 +1,18 @@
 class Freeze < Formula
   desc "Persistent Storage for Ice Objects"
   homepage "https://zeroc.com"
-  url "https://github.com/zeroc-ice/freeze.git", :tag => "v3.7.4"
+  url "https://github.com/zeroc-ice/freeze.git", :tag => "v3.7.5"
 
   bottle do
     root_url "https://zeroc.com/download/homebrew/bottles"
     cellar :any_skip_relocation
-    sha256 "5f3992cdf3fd49088580bed60627b0c51cbdee44953a84a3f2047e1ff1e352dc" => :catalina
+    sha256 "dc50dbd42e3584afc550df0710fd9646662eebefed67ffa7a4ae6cfe2c205d4e" => :big_sur
   end
 
   depends_on "zeroc-ice/tap/ice"
   depends_on "zeroc-ice/tap/berkeley-db@5.3"
 
   def install
-    ENV.O2 # Os causes performance issues
     args = [
       "prefix=#{prefix}",
       "V=1",
