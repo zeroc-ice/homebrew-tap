bcde70f78c9823979a87210a23c7f2c816ed0a66
Joe George <joe@zeroc.com>
2021-01-27 15:24:13 -0500

    Fix icegridgui hash

--- Casks/icegridgui.rb
+++ Casks/icegridgui.rb
@@ -1,6 +1,6 @@
 cask 'icegridgui' do
   version '3.7.5'
-  sha256 '6055b2d4507c67e592ecf4cecf5aef629164f56983141e116ea11d19c15f9e18'
+  sha256 'a594e5bec4aa0a5b495ed0c89af1c7234078959c5414acc0200bcb3908a47c6d'
 
   url "https://zeroc.com/download/ice/3.7/IceGridGUI-3.7.5.dmg"
   name 'IceGrid GUI'
