3eb7213511a40dd6f5cc3bbdabef47759a3cd917
Joe George <joe@zeroc.com>
2023-03-24 15:21:28 -0400

    ice 3.7.9

--- Formula/ice.rb
+++ Formula/ice.rb
@@ -1,39 +1,29 @@
 class Ice < Formula
   desc "Comprehensive RPC framework"
   homepage "https://zeroc.com"
-  url "https://github.com/zeroc-ice/ice/archive/v3.7.8.tar.gz"
-  sha256 "f2ab6b151ab0418fab30bafc2524d9ba4c767a1014f102df88d735fc775f9824"
+  url "https://github.com/zeroc-ice/ice/archive/v3.7.9.tar.gz"
+  sha256 "960b51bb14a0c89d60c0e65cb1d4c6b09fe94d4e4c033c50254f7cc9c862d3c0"
 
   bottle do
     root_url "https://download.zeroc.com/homebrew/bottles"
-    sha256 cellar: :any, arm64_monterey: "0c4940d22da94dfaacfad085285f733551d6a3aa3b0a84132f545cfc54ff6e44"
-    sha256 cellar: :any, monterey: "8bc0126a4deee6cb67fb1b17f2e859dcca3542da735ef1f29f3c5908a284b306"
+    sha256 cellar: :any, arm64_ventura: "9c156c8f9830ac43a00bd99af77497ec923ed734c4a7b84cc1a6916c212e1540"
   end
 
-  option "with-java", "Build the Ice for Java jar files"
-  option "without-xcode-sdk", "Build without the Xcode SDK for iOS development"
-
   depends_on "lmdb"
-  depends_on macos: :catalina
   depends_on "mcpp"
-  depends_on "openjdk@11" => :optional
 
   def install
-    # Ensure Gradle uses a writable directory even in sandbox mode
-    ENV["GRADLE_USER_HOME"] = "#{buildpath}/.gradle"
-
     args = [
       "prefix=#{prefix}",
       "V=1",
-      "USR_DIR_INSTALL=yes",
+      "USR_DIR_INSTALL=yes", # ensure slice and man files are installed to share
       "MCPP_HOME=#{Formula["mcpp"].opt_prefix}",
       "LMDB_HOME=#{Formula["lmdb"].opt_prefix}",
-      "CONFIGS=shared cpp11-shared #{build.with?("xcode-sdk") ? "xcodesdk cpp11-xcodesdk" : ""}",
+      "CONFIGS=all",
       "PLATFORMS=all",
       "SKIP=slice2confluence",
-      "LANGUAGES=cpp objective-c #{build.with?("java") ? "java java-compat" : ""}",
+      "LANGUAGES=cpp objective-c",
     ]
-
     system "make", "install", *args
 
     (libexec/"bin").mkpath
@@ -86,15 +76,15 @@ class Ice < Formula
     system "xcrun", "clang++", "-DICE_CPP11_MAPPING", "-std=c++11", "-c", "-I#{include}", "-I.", "Test.cpp"
     system "xcrun", "clang++", "-L#{lib}", "-o", "test", "Test.o", "Hello.o", "-lIce++11"
     system "./test"
-    if File.file?("#{lib}/IceSDK/bin/slice2cpp")
-      system "#{lib}/IceSDK/bin/slice2cpp", "Hello.ice"
-      system "xcrun", "--sdk", "macosx", "clang++", "-DICE_CPP11_MAPPING", "-std=c++11", "-c", \
-        "-I#{lib}/IceSDK/macosx.sdk/usr/include", "-I.", "Hello.cpp"
-      system "xcrun", "--sdk", "macosx", "clang++", "-DICE_CPP11_MAPPING", "-std=c++11", "-c", \
-        "-I#{lib}/IceSDK/macosx.sdk/usr/include", "-I.", "Test.cpp"
-      system "xcrun", "--sdk", "macosx", "clang++", "-L#{lib}/IceSDK/macosx.sdk/usr/lib", "-o", "test-sdk", \
-        "Test.o", "Hello.o", "-lIce++11", "-framework", "Security", "-framework", "Foundation", "-lbz2", "-liconv"
-      system "./test-sdk"
-    end
+    # Test the iOS SDK
+    system "#{bin}/slice2cpp", "Hello.ice"
+    system "xcrun", "--sdk", "macosx", "clang++", "-DICE_CPP11_MAPPING", "-std=c++11", "-c", \
+            "-I#{prefix}/sdk/macosx.sdk/usr/include", "-I.", "Hello.cpp"
+    system "xcrun", "--sdk", "macosx", "clang++", "-DICE_CPP11_MAPPING", "-std=c++11", "-c", \
+            "-I#{prefix}/sdk/macosx.sdk/usr/include", "-I.", "Test.cpp"
+    system "xcrun", "--sdk", "macosx", "clang++", "-L#{prefix}/sdk/macosx.sdk/usr/lib", "-o", "test-sdk", \
+            "Test.o", "Hello.o", "-lIce++11", "-framework", "Security", "-framework", "Foundation", \
+            "-lbz2", "-liconv"
+    system "./test-sdk"
   end
 end
