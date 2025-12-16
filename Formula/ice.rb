16215d3971db09e9da8627f7fb187c06d8f07d74
Joe George <joe@zeroc.com>
2025-12-16 14:56:37 -0500

    Review fixes

--- Formula/ice.rb
+++ Formula/ice.rb
@@ -13,56 +13,56 @@ class Ice < Formula
   depends_on "mcpp"
 
   def install
-      args = [
-        "prefix=#{prefix}",
-        "V=1",
-        "USR_DIR_INSTALL=yes", # ensure slice and man files are installed to share
-        "MCPP_HOME=#{Formula["mcpp"].opt_prefix}",
-        "LMDB_HOME=#{Formula["lmdb"].opt_prefix}",
-        "CONFIGS=all",
-        "PLATFORMS=all",
-        "LANGUAGES=cpp",
-      ]
-      system "make", "install", *args
+    args = [
+      "prefix=#{prefix}",
+      "V=1",
+      "USR_DIR_INSTALL=yes", # ensure slice and man files are installed to share
+      "MCPP_HOME=#{Formula["mcpp"].opt_prefix}",
+      "LMDB_HOME=#{Formula["lmdb"].opt_prefix}",
+      "CONFIGS=all",
+      "PLATFORMS=all",
+      "LANGUAGES=cpp",
+    ]
+    system "make", "install", *args
 
-      (libexec/"bin").mkpath
-      mv bin/"slice2py", libexec/"bin"
-    end
-
-    test do
-      (testpath / "Hello.ice").write <<~EOS
-        module Test
-        {
-            interface Hello
-            {
-                void sayHello();
-            }
-        }
-      EOS
-      (testpath / "Test.cpp").write <<~EOS
-        #include "Hello.h"
-        #include <Ice/Ice.h>
-
-        class HelloI : public Test::Hello
-        {
-        public:
-            void sayHello(const Ice::Current&) override {}
-        };
-
-        int main(int argc, char* argv[])
-        {
-          Ice::CommunicatorHolder ich(argc, argv);
-          auto adapter = ich->createObjectAdapterWithEndpoints("Hello", "default -h localhost");
-          adapter->add(std::make_shared<HelloI>(), Ice::stringToIdentity("hello"));
-          adapter->activate();
-          return 0;
-        }
-      EOS
-
-      system "#{bin}/slice2cpp", "Hello.ice"
-      system "xcrun", "clang++", "-std=c++20", "-c", "-I#{include}", "Hello.cpp"
-      system "xcrun", "clang++", "-std=c++20", "-c", "-I#{include}", "Test.cpp"
-      system "xcrun", "clang++", "-L#{lib}", "-o", "test", "Test.o", "Hello.o", "-lIce", "-lpthread"
-      system "./test"
-    end
+    (libexec/"bin").mkpath
+    mv bin/"slice2py", libexec/"bin"
   end
+
+  test do
+    (testpath / "Hello.ice").write <<~EOS
+      module Test
+      {
+          interface Hello
+          {
+              void sayHello();
+          }
+      }
+    EOS
+    (testpath / "Test.cpp").write <<~EOS
+      #include "Hello.h"
+      #include <Ice/Ice.h>
+
+      class HelloI : public Test::Hello
+      {
+      public:
+          void sayHello(const Ice::Current&) override {}
+      };
+
+      int main(int argc, char* argv[])
+      {
+        Ice::CommunicatorHolder ich(argc, argv);
+        auto adapter = ich->createObjectAdapterWithEndpoints("Hello", "default -h localhost");
+        adapter->add(std::make_shared<HelloI>(), Ice::stringToIdentity("hello"));
+        adapter->activate();
+        return 0;
+      }
+    EOS
+
+    system "#{bin}/slice2cpp", "Hello.ice"
+    system "xcrun", "clang++", "-std=c++20", "-c", "-I#{include}", "Hello.cpp"
+    system "xcrun", "clang++", "-std=c++20", "-c", "-I#{include}", "Test.cpp"
+    system "xcrun", "clang++", "-L#{lib}", "-o", "test", "Test.o", "Hello.o", "-lIce", "-lpthread"
+    system "./test"
+  end
+end
