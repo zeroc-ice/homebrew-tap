59f9baa0ce3a034a2be83dd650ec600729458e6e
Joe George <joe@zeroc.com>
2026-02-03 10:25:29 -0500

    Update icegridgui@3.7 to 3.7.11

--- Casks/icegridgui@3.7.rb
+++ Casks/icegridgui@3.7.rb
@@ -1,8 +1,8 @@
 cask "icegridgui@3.7" do
-  version "3.7.10"
-  sha256 "0dccb563b5d90eb0825dc1c3540709a4bed82c64da20860a18bfcd435fa7a186"
+  version "3.7.11"
+  sha256 "4a4528be2c60becadea1ce6377cf7a3191ad50dfbc7ca7aaf2799a8f8f03db70"
 
-  url "https://download.zeroc.com/ice/3.7/IceGridGUI-3.7.10.dmg"
+  url "https://download.zeroc.com/ice/3.7/IceGridGUI-3.7.11.dmg"
   name "IceGrid GUI"
   homepage "https://zeroc.com/"
 
