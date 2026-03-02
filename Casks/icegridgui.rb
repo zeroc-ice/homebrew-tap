ef4a3e39438b68b98f8740ad24b5ec638ccd5f6e
Joe George <joe@zeroc.com>
2026-03-02 14:31:28 -0500

    ice 3.8.1

--- Casks/icegridgui.rb
+++ Casks/icegridgui.rb
@@ -1,8 +1,8 @@
 cask "icegridgui" do
-  version "3.8.0"
-  sha256 "04ecd15c8c561c4e3d8b467e26a77d749f064af1a16af5d2fd7c40eaabd77fa9"
+  version "3.8.1"
+  sha256 "10c6386a4d67a4d5a5c34297159bad97fff7adaa5f50a0977a988d7164614927"
 
-  url "https://download.zeroc.com/ice/3.8/IceGridGUI-3.8.0.dmg"
+  url "https://download.zeroc.com/ice/3.8/IceGridGUI-3.8.1.dmg"
   name "IceGrid GUI"
   homepage "https://zeroc.com/"
 
