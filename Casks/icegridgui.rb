f93f7e2b5734a26c2d369b650e71aea7d227be7f
Joe George <joe@zeroc.com>
2026-03-18 08:20:04 -0400

    Add CI workflows and improve cask definitions (#13)

--- Casks/icegridgui.rb
+++ Casks/icegridgui.rb
@@ -2,9 +2,14 @@ cask "icegridgui" do
   version "3.8.1"
   sha256 "10c6386a4d67a4d5a5c34297159bad97fff7adaa5f50a0977a988d7164614927"
 
-  url "https://download.zeroc.com/ice/3.8/IceGridGUI-3.8.1.dmg"
+  url "https://download.zeroc.com/ice/3.8/IceGridGUI-#{version}.dmg"
   name "IceGrid GUI"
+  desc "Graphical administration tool for IceGrid"
   homepage "https://zeroc.com/"
+
+  livecheck do
+    skip "Versions are managed manually"
+  end
 
   app "IceGrid GUI.app"
 end
--- Casks/icegridgui@3.7.rb
+++ Casks/icegridgui@3.7.rb
@@ -2,9 +2,14 @@ cask "icegridgui@3.7" do
   version "3.7.11"
   sha256 "4a4528be2c60becadea1ce6377cf7a3191ad50dfbc7ca7aaf2799a8f8f03db70"
 
-  url "https://download.zeroc.com/ice/3.7/IceGridGUI-3.7.11.dmg"
+  url "https://download.zeroc.com/ice/3.7/IceGridGUI-#{version}.dmg"
   name "IceGrid GUI"
+  desc "Graphical administration tool for IceGrid"
   homepage "https://zeroc.com/"
+
+  livecheck do
+    skip "Versions are managed manually"
+  end
 
   app "IceGrid GUI.app"
 end
