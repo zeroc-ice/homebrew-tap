fb890eaa4e9ae29245a0f7a516ab912a6fc89253
Joe George <joe@externl.com>
2026-06-28 12:24:22 -0400

    Make ice@3.8 a real formula so versioned installs pass tap trust
    Replace the Aliases/ice@3.8 symlink with a real Formula/ice@3.8.rb
    (identical to ice.rb apart from the versioned class name). brew only
    auto-trusts a fully-qualified install argument when it names a real
    formula file, so the alias caused 'Refusing to load formula
    zeroc-ice/tap/ice from untrusted tap'. Fixes #16.

---
+++ Formula/ice@3.8.rb
@@ -0,0 +1,72 @@
+class IceAT38 < Formula
+  desc "Comprehensive RPC framework"
+  homepage "https://zeroc.com"
+  url "https://github.com/zeroc-ice/ice/archive/v3.8.2.tar.gz"
+  sha256 "d350ebbcdd7971fafccebfdf1e99db139dc6d121f5a5dcdc4036256206735078"
+
+  bottle do
+    root_url "https://download.zeroc.com/ice/3.8"
+    sha256 cellar: :any, arm64_tahoe: "01103d6b9219e76b6276aab1c7541bdb259928aaeefa41ca82e8d6e315acc0f0"
+    sha256 cellar: :any, arm64_sequoia: "240aef8fa07f3eec7bad82741b6a297092bdde2ceeeae665242e9b7ea01bcadb"
+  end
+
+  depends_on "lmdb"
+  depends_on "mcpp"
+
+  def install
+    args = [
+      "prefix=#{prefix}",
+      "V=1",
+      "USR_DIR_INSTALL=yes", # ensure slice and man files are installed to share
+      "MCPP_HOME=#{Formula["mcpp"].opt_prefix}",
+      "LMDB_HOME=#{Formula["lmdb"].opt_prefix}",
+      "CONFIGS=all",
+      "PLATFORMS=all",
+      "LANGUAGES=cpp",
+    ]
+    system "make", "install", *args
+
+    (libexec/"bin").mkpath
+    mv bin/"slice2py", libexec/"bin"
+  end
+
+  test do
+    (testpath / "Hello.ice").write <<~EOS
+      module Test
+      {
+          interface Hello
+          {
+              void sayHello();
+          }
+      }
+    EOS
+
+    port = free_port
+
+    (testpath / "Test.cpp").write <<~CPP
+      #include "Hello.h"
+      #include <Ice/Ice.h>
+
+      class HelloI : public Test::Hello
+      {
+      public:
+          void sayHello(const Ice::Current&) override {}
+      };
+
+      int main(int argc, char* argv[])
+      {
+        Ice::CommunicatorHolder ich(argc, argv);
+        auto adapter = ich->createObjectAdapterWithEndpoints("Hello", "default -h 127.0.0.1 -p #{port}");
+        adapter->add(std::make_shared<HelloI>(), Ice::stringToIdentity("hello"));
+        adapter->activate();
+        return 0;
+      }
+    CPP
+
+    system "#{bin}/slice2cpp", "Hello.ice"
+    system ENV.cxx, "-std=c++20", "-c", "-I#{include}", "Hello.cpp"
+    system ENV.cxx, "-std=c++20", "-c", "-I#{include}", "Test.cpp"
+    system ENV.cxx, "-L#{lib}", "-o", "test", "Test.o", "Hello.o", "-lIce", "-lpthread"
+    system "./test"
+  end
+end
